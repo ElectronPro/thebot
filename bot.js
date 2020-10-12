eb965f8b85582e293b71162eebbbeb02b20cb985
ElectronPro <71539127+ElectronPro@users.noreply.github.com>
2020-10-12 19:20:57 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.on('ready', () => {
+
+console.log("ready for action");
+
+})
+
+Client.on('message', msg=> {
+
+if(msg.content ===  "hi"
+msg.reply('hello User')
+
+}
+
+
+)}
+
+
+
+
+Client.login(process.env.BOT_TOKEN);
+
+
